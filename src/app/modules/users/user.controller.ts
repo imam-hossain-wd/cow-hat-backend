a495bd75ce47588b0ad282af318a3ca935d26662
imam-hossain <zunayedemon6t9@gmail.com>
2023-06-16 02:51:13 +0600

    setup a custom sendResponse function for response

--- src/app/modules/users/user.controller.ts
+++ src/app/modules/users/user.controller.ts
@@ -1,22 +1,29 @@
 import { Request, Response } from 'express';
 import { userService } from './user.service';
 import { errorlogger } from '../../../shared/logger';
+import { IUser } from './user.interface';
+import httpStatus from 'http-status';
+import sendResponse from '../../../shared/sendResponse';
+
+
 
 const createUser = async (req: Request, res: Response) => {
   try {
     const user = req.body;
     const result = await userService.createUser(user);
-    
-    res.status(200).json({
-        success: true,
-        statusCode:200,
-        message: 'user created successfully!',
-        data: result,
-      });
+
+    sendResponse<IUser>(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'user created successfully!',
+      data: result,
+    });
   } catch (error) {
     errorlogger.error(error);
   }
 };
+
+
 
 export const userController = {
   createUser,
