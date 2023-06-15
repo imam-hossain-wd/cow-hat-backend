a60ec30121073a18fcc16f8306ff20ec914ac5ee
imam-hossain <zunayedemon6t9@gmail.com>
2023-06-15 22:13:34 +0600

    eslint & prettier setup successfully

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,10 @@
 import express, { Application, urlencoded } from 'express';
 import cors from 'cors';
 
-const app:Application = express();
+const app: Application = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(urlencoded({extended:true}));
-
-
-
+app.use(urlencoded({ extended: true }));
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -3,21 +3,19 @@ import app from './app';
 import config from './config/index';
 import { errorlogger, logger } from './shared/logger';
 
-const {port, database_url} = config
+const { port, database_url } = config;
 
+const run = async () => {
+  try {
+    await mongoose
+      .connect(database_url as string)
+      .then(() => logger.info('database connected successfully'));
 
-const run = async()=> {
-    try{
-    await mongoose.connect(database_url as string)
-    .then(()=> logger.info('database connected successfully'))
-
-    app.listen(port , ()=> {
-        logger.info(`server is running on port ${port}`);
-    })
-
-    }
-    catch(error){
-        errorlogger.error(error);
-    }
-}
-run().catch(error => errorlogger.error(error))
+    app.listen(port, () => {
+      logger.info(`server is running on port ${port}`);
+    });
+  } catch (error) {
+    errorlogger.error(error);
+  }
+};
+run().catch(error => errorlogger.error(error));
