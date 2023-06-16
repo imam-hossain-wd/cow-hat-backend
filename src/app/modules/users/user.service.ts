839298c519fd1b82fe7ffd9c97e9d6c308d1464f
imam-hossain <zunayedemon6t9@gmail.com>
2023-06-17 01:35:54 +0600

    validate user.service.ts

--- src/app/modules/users/user.service.ts
+++ src/app/modules/users/user.service.ts
@@ -1,5 +1,7 @@
 // import httpStatus from 'http-status';
 // import ApiError from '../../../errors/ApiError';
+import httpStatus from 'http-status';
+import ApiError from '../../../errors/ApiError';
 import { IUser } from './user.interface';
 import { User } from './user.model';
 
@@ -10,10 +12,17 @@ const createUser = async (user: IUser): Promise<IUser | null> => {
 
 const getAllUsers = async () => {
   const allUsers = await User.find({});
+  if(allUsers.length === 0){
+    throw new ApiError(httpStatus.NOT_FOUND, "There is no user found")
+}
   return allUsers;
 };
 
 const getSingleUser = async (id: string): Promise<IUser | null> => {
+  const isexits = await User.findOne({_id: id})
+  if(!isexits){
+    throw new ApiError(httpStatus.NOT_FOUND, "user is not found")
+  }
   const singleUser = await User.findOne({ _id: id });
   return singleUser;
 };
