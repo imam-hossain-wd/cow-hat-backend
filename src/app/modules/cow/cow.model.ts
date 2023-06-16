cc53f38cb2553d3bd26aed868a547866b171e728
imam-hossain <zunayedemon6t9@gmail.com>
2023-06-17 00:40:39 +0600

    create a cow model

---
+++ src/app/modules/cow/cow.model.ts
@@ -0,0 +1,50 @@
+import { Schema, model, Types } from 'mongoose';
+import { Breed, Category, ICow, Label } from './cow.interface';
+
+const cowSchema = new Schema<ICow>({
+  name: { 
+    type: String,
+     required: true 
+    },
+  age: { 
+    type: Number,
+     required: true
+     },
+  price: { 
+    type: Number,
+     required: true 
+    },
+  location: { 
+    type: String, 
+    enum: Object.values(Location), 
+    required: true 
+},
+  breed: { 
+    type: String, 
+    enum: Object.values(Breed), 
+    required: true 
+},
+  weight: { 
+    type: Number, 
+    required: true
+ },
+  label: { 
+    type: String, 
+    enum: Object.values(Label),
+     default: Label.ForSale
+     },
+  category: {
+     type: String, 
+     enum: Object.values(Category),
+      required: true 
+    },
+  seller: { 
+    type: Types.ObjectId, 
+    ref: 'User', required: true 
+},  
+});
+
+const Cow= model<ICow>('Cow', cowSchema);
+
+export default Cow;
+
